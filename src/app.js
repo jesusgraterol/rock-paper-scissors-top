b8bec16edb382dd22d3e3c235d9d273b4325dacc
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-09-08 15:42:40 -0400

    Basic GUI Completed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 /**
  * Application Instance
  * The instance of the game is initializes and ready to be started.
@@ -13,33 +8,66 @@ class App {
 
     // The win counters
     #win_counts;
+    
+    // The game's instructions element
+    #instructions_el;
+
+    // The game's score element
+    #score_el;
+
+    // The game's start button
+    #start_button_container_el;
+    #start_button_el;
+
+    // The player selection container and options
+    #player_selection_container_el;
+    #player_selection_options_el;
+
+    // The winner container
+    #winner_container_el;
+
+    // The matches container
+    #matches_container_el;
+
 
 
     constructor () {
+        // Initialize the matches payload
+        this.#matches = [];
 
-    }
+        // Initialize the win counters
+        this.#win_counts = { player: 0, computer: 0 };
 
+        // Init the instructions element
+        this.#instructions_el = document.getElementById("instructions");
 
+        // Init the score element and hide it as the game has not started
+        this.#score_el = document.getElementById("score");
+        this.#score_el.style.display = "none";
 
-
-
-
-    /**
-     * Starts a game that will run a number of matches, declare
-     * a winner and show the game summary at the end.
-     * @param match_num 
-     */
-    start_game(match_num = 5) {
-        // Reset the game data
-        this.#reset_game_data();
-
-        // Play until the match limit has been reached
-        while (this.#matches.length < match_num) {
-            this.#play_match();
-        }
+        // Init the start button element and attach an event listener
+        this.#start_button_container_el = document.getElementById("start_button_container");
+        this.#start_button_el = document.getElementById("start_button");
+        this.#start_button_el.addEventListener("click", () => this.start_game());
         
-        // Finally, display the game's result
-        this.#display_game_result();
+        // Init the player selection elements and attach an event listener
+        this.#player_selection_container_el = document.getElementById("player_selection_container");
+        this.#player_selection_options_el = document.getElementById("player_selection_options");
+        this.#player_selection_container_el.style.display = "none";
+        this.#player_selection_options_el.addEventListener("click", (e) => {
+            const choice = e.target.innerText;
+            if (choice != "ROCK" && choice != "PAPER" && choice != "SCISSORS") return;
+            this.#play_match(choice);
+        });
+        
+        // Init the winner's container
+        this.#winner_container_el = document.getElementById("winner_container");
+        this.#winner_container_el.style.display = "none";
+
+        // Init the matches container
+        this.#matches_container_el = document.getElementById("matches_container");
+        this.#matches_container_el.style.display = "none";
+
     }
 
 
@@ -47,54 +75,113 @@ class App {
 
 
 
+    /**
+     * Starts the game by hiding and showing the appropiate elements
+     */
+    start_game() {
+        // Replace the instructions with the game's score
+        this.#instructions_el.style.display = "none";
+        this.#score_el.style.display = "block";
+        this.#set_score();
+
+        // Hide the start button
+        this.#start_button_container_el.style.display = "none";
+
+        // Display the player's selection box
+        this.#player_selection_container_el.style.display = "block";
+
+        // Display the matches container
+        this.#matches_container_el.style.display = "block";
+    }
+
+
+
+
 
 
     /**
-     * Gathers both choices from the player and the computer and 
-     * evaluates them. If there is a winner, it processes the
-     * information so it can be displayed at the end of the game.
+     * When the player chooses a weapon, a random choice is generated for the
+     * computer and both weapons are evaluated. Once the player or the computer
+     * reaches 5 victories, the game stops and the winner is set.
      */
-    #play_match() {
-        // Gather both choices
-        const player_choice = this.#get_player_choice();
+    #play_match(player_choice) {
+        // The computer chooses its weapon
         const computer_choice = this.#get_computer_choice();
 
         // Calculate the winner of the match
         const winner = this.#calculate_winner(player_choice, computer_choice);
 
-        // Process the results if it wasn't a tie
-        if (winner != "tie") {
-            this.#win_counts[winner] += 1;
-            this.#matches.push({
-                player_choice: player_choice,
-                computer_choice: computer_choice,
-                winner: winner
-            });
-        }
+        // Increment the counter if it isn't a tie
+        if (winner != "tie") this.#win_counts[winner] += 1;
 
-        // Print the match's result
-        this.#display_match_result(player_choice, computer_choice, winner);
+        // Put together the match record and add it to the list
+        this.#matches.push({
+            player_choice: player_choice,
+            computer_choice: computer_choice,
+            winner: winner
+        });
+
+        // Handle the match post actions
+        this.#on_match_played();
     }
 
 
 
 
+    /**
+     * Triggers whenever a match is played and handles the after effects of the match. 
+     * It also handles the end of the game.
+     */
+    #on_match_played() {
+        // Add the match to the game payload
+        this.#matches_container_el.innerHTML = this.#matches.reduce(
+            (accum, current) => accum += this.#build_match_record_content(current), 
+            ""
+        );
+
+        // Update the score
+        this.#set_score();
+
+        // Check if the game ended
+        if (this.#win_counts.player == 5 || this.#win_counts.computer == 5) {
+            // Hide the player's selection box
+            this.#player_selection_container_el.style.display = "none";
+
+            // Set & display the winner
+            this.#winner_container_el.innerText = 
+                `${this.#win_counts.player == 5 ? "PLAYER": "COMPUTER"} WINS!`;
+            this.#winner_container_el.style.display = "block";
+        }
+    }
 
 
 
+    /**
+     * Puts together the HTML content for a match record.
+     * @param record 
+     * @returns string
+     */
+    #build_match_record_content(record) {
+        let match_content;
+        if (record.winner == "tie") {
+            match_content = `
+                <p>Nobody Wins</p>
+                <p>${record.player_choice} (Player) does not beat ${record.computer_choice} (Computer)</p>
+            `;
+        } else if (record.winner == "player") {
+            match_content = `
+                <p>Player Wins!</p>
+                <p>${record.player_choice} (Player) beats ${record.computer_choice} (Computer)</p>
+            `;
+        } else {
+            match_content = `
+                <p>Computer Wins!</p>
+                <p>${record.computer_choice} (Computer) beats ${record.player_choice} (Player)</p>
+            `;
+        }
+        return `<div>${match_content}<hr></div>`
+    }
 
-
-
-
-
-
-
-
-
-
-    /****************************
-     * Player & Computer Choice *
-     ****************************/
 
 
 
@@ -109,63 +196,15 @@ class App {
 
         // Select a choice based on the value
         if (random_number >= 0.66) {
-            return "rock";
+            return "ROCK";
         } else if (random_number >= 0.33) {
-            return "paper";
+            return "PAPER";
         } else {
-            return "scissors";
+            return "SCISSORS";
         }
     }
 
 
-
-
-
-
-    /**
-     * Asks the player to input their choice for the active match persistently.
-     * @returns string
-     */
-    #get_player_choice() {
-        // Initialize the player's choice
-        let player_choice = "";
-
-        // Iterate until a valid input is provided
-        while (!this.#is_choice_valid(player_choice)) {
-            player_choice = prompt("Enter your choice (rock, paper or scissors)").toLowerCase();
-        }
-
-        // Finally, return the choice
-        return player_choice;
-    }
-
-
-
-
-    /**
-     * Verifies if a given choice is valid.
-     * @param choice 
-     * @returns boolean
-     */
-    #is_choice_valid(choice) {
-        return  typeof(choice) == "string" && 
-                (choice == "rock" || choice == "paper" || choice == "scissors");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    /****************
-     * Misc Helpers *
-     ****************/
 
 
 
@@ -188,9 +227,9 @@ class App {
 
         // Check if the player won
         else if (
-            (player_choice == "paper" && computer_choice == "rock") ||
-            (player_choice == "rock" && computer_choice == "scissors") ||
-            (player_choice == "scissors" && computer_choice == "paper")
+            (player_choice == "PAPER" && computer_choice == "ROCK") ||
+            (player_choice == "ROCK" && computer_choice == "SCISSORS") ||
+            (player_choice == "SCISSORS" && computer_choice == "PAPER")
         ) {
             return "player";
         }
@@ -207,62 +246,15 @@ class App {
 
 
 
-    /**
-     * Displays the result of a single match.
-     * @param player_choice 
-     * @param computer_choice 
-     * @param winner 
-     */
-    #display_match_result(player_choice, computer_choice, winner) {
-        if (winner == "tie") { 
-            console.log(`\n\nTIE: Both, the player and the computer picked: ${player_choice}`);
-        } else if (winner == "player") {
-            console.log(`\n\nPLAYER WINS! ${player_choice} beats ${computer_choice}\n`);
-        } else {
-            console.log(`\n\nCOMPUTER WINS! ${computer_choice} beats ${player_choice}\n`);
-        }
-    }
-
-
-
-
 
 
 
     /**
-     * Displays the game results once all the matches have been played.
+     * Sets the current score on the app's heading.
      */
-    #display_game_result() {
-        console.clear();
-        console.log("Rock Paper Scissors Championship\n");
-        console.log(`${this.#win_counts.player > this.#win_counts.computer ? "Player": "Computer"} Wins!\n\n`);
-
-        console.log("Final Scores:");
-        console.log(`Player: ${this.#win_counts.player}`);
-        console.log(`Computer: ${this.#win_counts.computer}\n\n`);
-
-        console.log("Matches:");
-        for (let i = 0; i < this.#matches.length; i++) {
-            console.log(`\n\nMatch #${i + 1}'s Winner: ${this.#matches[i].winner}`);
-            if (this.#matches[i].winner == "player") {
-                console.log(`${this.#matches[i].player_choice} beats ${this.#matches[i].computer_choice}\n`);
-            } else {
-                console.log(`${this.#matches[i].computer_choice} beats ${this.#matches[i].player_choice}\n`);
-            }
-        }
-    }
-
-
-
-
-
-    /**
-     * Resets the game's data so the "Play Again" flow can be implemented.
-     */
-    #reset_game_data() {
-        console.clear();
-        this.#matches = [];
-        this.#win_counts = { player: 0, computer: 0 };
+    #set_score() {
+        this.#score_el.innerText = 
+            `PLAYER: ${this.#win_counts.player} | COMPUTER: ${this.#win_counts.computer}`;
     }
 }
 
@@ -277,4 +269,3 @@ class App {
  * DO NOT MODIFY THIS CODE
  */
 const app = new App();
-app.start_game(); // <- @DEPRECATE WHEN THE GUI IS IMPLEMENTED
